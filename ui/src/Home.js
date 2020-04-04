de1e2d82ea8d66441d63ac5b60ff78c85156321d
Ryan Christilaw <ryan.christilaw@gmail.com>
2020-04-04 19:39:28 -0400

    Update UI

--- ui/src/Home.js
+++ ui/src/Home.js
@@ -1,16 +1,31 @@
 import React from "react";
 import NotePad from "./notes/Notepad";
+import StockCentral from "./stocks/StockCentral";
+
+
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-        };
+    }
+
+    getCurrentPage = () => {
+        const currentRoute = this.props.route;
+
+        if (currentRoute === "STOCKS") {
+            return <StockCentral/>
+        }
+        else {
+            return <NotePad/>
+        }
     }
 
     render() {
+        const currentPage = this.getCurrentPage();
         return (
-            <NotePad/>
+            <div>
+                {currentPage}
+            </div>
         );
     }
 }
---
+++ ui/src/stocks/StockCentral.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import './StockCentral.css';
+
+class StockCentral extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="stock-central">
+                STOCKS
+            </div>
+        );
+    }
+}
+
+export default StockCentral;
